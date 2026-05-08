ed981960790afa96f07a4871fadb00bd40c5a81f
Euruviel <euruviel.marquez@tecdesoftware.edu.mx>
2026-05-07 18:50:05 -0600

    Mejoras de implementación y de diseño en catalogo y Agregar

--- Models/Item.cs
+++ Models/Item.cs
@@ -6,7 +6,11 @@
         public string Titulo { get; set; }
         public string Genero { get; set; }
         public int Ano { get; set; }
-        public string Consola { get; set; }
+        public string Estudio { get; set; }
+        public string Estado { get; set; }
+        public int Calificacion { get; set; }
         public string Descripcion { get; set; }
+        public string Consola { get; set; }
+        
     }
 }
